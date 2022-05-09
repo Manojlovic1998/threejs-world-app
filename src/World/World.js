a99b106814016b20b86178bf7f1ee2ecf3a7e041
Nemanja <manojlovic911@hotmail.com>
2022-05-09 14:40:38 +0200

    Add createScene to class constructor

--- src/World/World.js
+++ src/World/World.js
@@ -1,7 +1,10 @@
+import { createScene } from "./components/scene.js";
+
 class World {
   // 1. Create an instance of the World app
   constructor(container) {
     this.sceneContainer = container;
+    this.scene = createScene();
   }
   // 2. Render the scene
   render() {}
