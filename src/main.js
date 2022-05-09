9f8288901b203427d61f2af623ead1236a370f8a
Nemanja <manojlovic911@hotmail.com>
2022-05-09 12:23:13 +0200

    Add interface workflow to the main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Scene wrapper container
+const container = document.getElementById("scene-container");
+
+// 1. Create an instance of the World app
+const world = new World(container);
+
+// 2. Rendeer the scene
+world.render();
