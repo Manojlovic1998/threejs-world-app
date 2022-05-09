14e780af801e97617cf3c5d5f4ce6ec2edd33683
Nemanja <manojlovic911@hotmail.com>
2022-05-09 12:31:55 +0200

    Create World class

---
+++ src/World/World.js
@@ -0,0 +1,10 @@
+class World {
+  // 1. Create an instance of the World app
+  constructor(container) {
+    this.sceneContainer = container;
+  }
+  // 2. Render the scene
+  render() {}
+}
+
+export default World;
---
+++ src/World/components/scene.js
@@ -0,0 +1,5 @@
+import { Scene } from "../../../vendor/three/build/three.module";
+
+export const createScene = () => {
+  return new Scene();
+};
