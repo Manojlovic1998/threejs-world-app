9b668d452dc9ef74dbd1bfa26142e66b4d79c800
Nemanja <manojlovic911@hotmail.com>
2022-05-09 20:12:43 +0200

    Add renderer to the World class

--- src/World/World.js
+++ src/World/World.js
@@ -1,4 +1,5 @@
 import { createCamera } from "./components/camera.js";
+import { createRenderer } from "./system/renderer.js";
 import { createScene } from "./components/scene.js";
 
 class World {
@@ -7,6 +8,7 @@ class World {
     this.sceneContainer = container;
     this.scene = createScene();
     this.camera = createCamera(container.clientWidth / container.clientHeight);
+    this.render = createRenderer();
   }
   // 2. Render the scene
   render() {}
