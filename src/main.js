051f19b147ccd779d8b236cba6e9a023f90f7028
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-10 20:20:08 +0200

    Test implementation of the phyCorrectLights switch

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ const main = () => {
   // Scene wrapper container
   const container = document.getElementById("scene-container");
 
+  // World configuration
+  let worldConfig = {
+    phyCorrectLights: true,
+  };
   // 1. Create an instance of the World app
-  const world = new World(container);
+  const world = new World(container, worldConfig);
 
   // 2. Renderer the scene
   world.resize(container);
