e2f3849da91316a72fd359245bc9800542cc945f
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-10 20:14:32 +0200

    Add switch for physically correct lights module

--- src/World/system/renderer.js
+++ src/World/system/renderer.js
@@ -1,7 +1,12 @@
 import { WebGLRenderer } from "../../../vendor/three/build/three.module.js";
 
-export const createRenderer = () => {
+export const createRenderer = (options) => {
   const renderer = new WebGLRenderer();
+
+  // Turn on the physically correct light model
+  if (options.phyCorrectLights) {
+    renderer.physicallyCorrectLights = options.phyCorrectLights;
+  }
 
   return renderer;
 };
