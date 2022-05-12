7f4a66ccb36a38baf3786cfc1b9f6c898d503438
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-12 19:58:05 +0200

    Add options key check for light and phyCorrectLights config

--- src/World/World.js
+++ src/World/World.js
@@ -2,20 +2,28 @@ import { createCamera } from "./components/camera.js";
 import { createRenderer } from "./system/renderer.js";
 import { createScene } from "./components/scene.js";
 import { createCube } from "./components/cube/cube.js";
+import { createLight } from "./components/lights.js";
 
 class World {
   // 1. Create an instance of the World app
-  constructor(container, options = { phyCorrectLights: false }) {
+  constructor(container, options) {
     this.sceneContainer = container;
+    this.options = options;
     this.scene = createScene();
     this.camera = createCamera(container.clientWidth / container.clientHeight);
-    this.renderer = createRenderer(options);
+    "light" in options
+      ? (this.light = createLight(options))
+      : (this.light = createLight());
+    this.scene.add(this.light);
+    "phyCorrectLights" in options
+      ? (this.renderer = createRenderer(options))
+      : (this.renderer = createRenderer());
     container.append(this.renderer.domElement);
   }
   // 2. Render the scene
   render(object) {
     if ((object = "cube")) {
-      const cube = createCube();
+      const cube = createCube(this.options);
       this.scene.add(cube);
     }
     // draw a single frame
