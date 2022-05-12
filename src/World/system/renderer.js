b114f340a6218ffef3e6413e00b73a90ba8185ed
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-12 19:51:55 +0200

    Add default arg to render and factorize cond

--- src/World/system/renderer.js
+++ src/World/system/renderer.js
@@ -1,10 +1,10 @@
 import { WebGLRenderer } from "../../../vendor/three/build/three.module.js";
 
-export const createRenderer = (options) => {
+export const createRenderer = (options = { phyCorrectLights: false }) => {
   const renderer = new WebGLRenderer();
 
   // Turn on the physically correct light model
-  if (options.phyCorrectLights) {
+  if ("phyCorrectLights" in options) {
     renderer.physicallyCorrectLights = options.phyCorrectLights;
   }
 
