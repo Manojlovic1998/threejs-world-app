edde2db6640f71ca03f099c5f8ab1434ae76ea7e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-12 19:50:37 +0200

    ADd material switch func to cube

--- src/World/components/cube/cube.js
+++ src/World/components/cube/cube.js
@@ -2,15 +2,23 @@ import {
   BoxBufferGeometry,
   Mesh,
   MeshBasicMaterial,
+  MeshStandardMaterial,
 } from "../../../../vendor/three/build/three.module.js";
 
-export const createCube = () => {
+export const createCube = (options = { material: "basic" }) => {
   // create a geometry
   const geometry = new BoxBufferGeometry(2, 2, 2);
 
-  // create basic material
-  const material = new MeshBasicMaterial();
-
+  let material;
+  // create material
+  switch (options.material) {
+    case "basic":
+      material = new MeshBasicMaterial();
+      break;
+    case "standard":
+      material = new MeshStandardMaterial();
+      break;
+  }
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
 
