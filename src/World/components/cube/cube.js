df9563aca57c53d67b333f766ec8d458fdaed87e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-12 22:54:55 +0200

    Add rotation functionality

--- src/World/components/cube/cube.js
+++ src/World/components/cube/cube.js
@@ -19,8 +19,14 @@ export const createCube = (options = { material: "basic" }) => {
       material = new MeshStandardMaterial();
       break;
   }
+
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
+
+  if ("rotation" in options.cube.transformation) {
+    let { x, y, z } = options.cube.transformation.rotation;
+    cube.rotation.set(x, y, z);
+  }
 
   return cube;
 };
